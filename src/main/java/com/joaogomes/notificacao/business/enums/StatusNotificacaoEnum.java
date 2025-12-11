678c710135f4f4646de5d33a16d1a2c7c42c6a16
João Victor <jvictor.sg@outlook.com>
2025-12-10 23:54:01 -0300

    Finalização do metodo de envio de e-mail

---
+++ src/main/java/com/joaogomes/notificacao/business/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,5 @@
+package com.joaogomes.notificacao.business.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE, NOTIFICADO, CANCELADO
+}
---
+++ src/main/java/com/joaogomes/notificacao/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.joaogomes.notificacao.controller;
+
+
+import com.joaogomes.notificacao.business.EmailService;
+import com.joaogomes.notificacao.business.dto.TarefasDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<Void> enviarEmail(@RequestBody TarefasDTO dto){
+        emailService.enviaEmail(dto);
+        return ResponseEntity.ok().build();
+    }
+
+}
